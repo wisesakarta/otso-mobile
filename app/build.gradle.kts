23ae38264810f8740d2a1bc251edd495dc8df9af
Karta Sena Wisesa <arpomuse@gmail.com>
2026-05-01 02:47:27 +0700

    chore(release): bump version to 2.2.0 for prod release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.otso.app"
         minSdk = 31
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.1.0"
+        versionCode = 5
+        versionName = "2.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
